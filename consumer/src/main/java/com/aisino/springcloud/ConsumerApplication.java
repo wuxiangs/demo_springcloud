c8409c6637ed17dbbe1194ee6e161d81191a49fa
wuxiang <15955429191@163.com>
2024-01-08 16:22:07 +0800

    提交

---
+++ consumer/src/main/java/com/aisino/springcloud/ConsumerApplication.java
@@ -0,0 +1,16 @@
+package com.aisino.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: wuxiang
+ * @create: 2024-01-08-14:43
+ */
+@SpringBootApplication
+public class ConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerApplication.class,args);
+    }
+}
---
+++ product/src/main/java/com/aisino/springcloud/ProductApplication.java
@@ -0,0 +1,16 @@
+package com.aisino.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: wuxiang
+ * @create: 2024-01-08-14:43
+ */
+@SpringBootApplication
+public class ProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductApplication.class,args);
+    }
+}
